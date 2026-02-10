b97e044dc14f60f7c16470314af95d9000520737
Jashan <jashanpreet.singh@marblex.ai>
2026-02-10 15:50:06 +0530

    Finished The Tutorial

--- main.js
+++ main.js
@@ -1,26 +1,22 @@
-const { app, BrowserWindow } = require('electron/main')
+const { app, BrowserWindow, ipcMain } = require('electron')
+
+const path = require('node:path')
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: true,
+            nodeIntegration: false
+        }
     })
-
+    win.webContents.openDevTools()
     win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
+    ipcMain.handle('ping', async ()=> 'pong')
     createWindow()
-
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow()
-        }
-    })
-})
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
 })
